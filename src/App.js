c8910c86ca47f568dc22cde8bdd85ae28f1f868f
DaramGuru95 <daramgurum33@gmail.com>
2026-01-05 12:08:54 +0530

    Configured Tailwindcss Points written to build Project

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <p>Hey Daram Guraiah</p>
+      <p className="text-3xl font-bold text-green-800 p-3 m-3">
+        Hey Daram Guraiah
+      </p>
     </div>
   );
 }
